d2105d38869fe3e2db0040a23edf7b91f23894c0
alexpeev9 <62713012+alexpeev9@users.noreply.github.com>
2021-01-17 12:39:22 +0200

    Backpack style and Header Fix Error

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,9 +1,9 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('location') }}'><i class='nav-icon la la-question'></i> Locations</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('ticket') }}'><i class='nav-icon la la-question'></i> Tickets</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('performance') }}'><i class='nav-icon la la-question'></i> Performances</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('location') }}'><i class='nav-icon  la la-circle'></i> Locations</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('ticket') }}'><i class='nav-icon  la la-circle'></i> Tickets</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('performance') }}'><i class='nav-icon la la-circle'></i> Performances</a></li>
 
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('user') }}'><i class='nav-icon la la-question'></i> Users</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('performance_locations') }}'><i class='nav-icon la la-question'></i> Perform-Locations</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('performance_tickets') }}'><i class='nav-icon la la-question'></i> Performa-Tickets</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('user') }}'><i class='nav-icon la la-circle'></i> Users</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('performance_locations') }}'><i class='nav-icon la la-circle'></i> P-Locations</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('performance_tickets') }}'><i class='nav-icon la la-circle'></i> P-Tickets</a></li>
